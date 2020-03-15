cfecb38a16c9e6a126a615aa217fa7db262efa7e
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-15 20:53:14 +0000

    Updated chartConfig.js

--- src/components/chart/chartConfig.js
+++ src/components/chart/chartConfig.js
@@ -1,29 +1,25 @@
-const gridLines = [
-	{
-		gridLines: { zeroLineColor: '#EEEEEE', zeroLineWidth: 1, lineWidth: 2, borderDash: [ 2, 5 ] },
-		ticks: {
-			beginAtZero: true,
-			fontColor: '#666666',
-			fontSize: 14
-		},
-		scaleLabel: {
-			display: true,
-			lineHeight: 3,
-			labelString: 'Values',
-			fontSize: 25,
-			fontColor: '#563D7C'
-		}
-	}
-];
+const gridLines = { zeroLineColor: '#EEEEEE', zeroLineWidth: 1, lineWidth: 2, borderDash: [ 2, 5 ] };
+const ticks = {
+	beginAtZero: true,
+	fontColor: '#666666',
+	fontSize: 14
+};
+const scaleLabel = {
+	display: true,
+	lineHeight: 3,
+	labelString: 'Time Range',
+	fontSize: 25,
+	fontColor: '#563D7C'
+};
 
 export const options = {
 	scales: {
-		xAxes: gridLines,
-		yAxes: gridLines
-    },
-    legend: {
-        display: false
-     },
+		xAxes: [ { gridLines, ticks, scaleLabel } ],
+		yAxes: [ { gridLines, ticks, scaleLabel: { ...scaleLabel, labelString: 'Values' } } ]
+	},
+	legend: {
+		display: false
+	},
 	tooltips: {
 		yAlign: 'bottom',
 		displayColors: false,
@@ -34,9 +30,9 @@ export const options = {
 export const dataSetsConfig = {
 	label: 'Data',
 	fill: false,
-    pointBackgroundColor: '#FF550B',
-    pointHoverBackgroundColor: '#FFFFFF',
-    pointBorderWidth: 3,
+	pointBackgroundColor: '#FF550B',
+	pointHoverBackgroundColor: '#FFFFFF',
+	pointBorderWidth: 3,
 	borderColor: '#FF550B',
 	borderWidth: 3
 };
